a2047005fce8470dba360f53376eaa25a4046580
Tacoalarmclock <league@imac-2.attlocal.net>
2019-03-12 18:31:07 -0700

    Array Cheat Sheet, Robot Race (IP)

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,52 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] stringArray = new String[5];
 		//2. print the third element in the array
-		
+		System.out.println(stringArray[2]);
 		//3. set the third element to a different value
-		
+		stringArray[2] = "String";
 		//4. print the third element again
-		
+		System.out.println(stringArray[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < stringArray.length; i++) {
+			System.out.println(stringArray[i]);
+		}
 		
+		System.out.println("~");
 		
 		//6. make an array of 50 integers
-		
+		int[] intArray = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		Random random = new Random();
+		for (int i = 0; i < intArray.length; i++) {
+			int r = random.nextInt(50);
+			intArray[i] = r;
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		int smallestInt = intArray[0];
+		for (int i = 0; i < intArray.length; i++) {
+			if(intArray[i]<smallestInt) {
+				smallestInt = intArray[i];
+			}
+		}
+		System.out.println(smallestInt);
+		//9. print the entire array to see if step 8 was correct
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		//10. print the largest number in the array
+		int largestInt = intArray[0];
+		for (int i = 0; i < intArray.length; i++) {
+			if(intArray[i]>largestInt) {
+				largestInt = intArray[i];
+			}
+		}
+		System.out.println(largestInt);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,46 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
+		// 2. create an array of 5 robots.
+		Robot[] robots = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+		for (int i = 0; i < robots.length; i++) {
+			robots[i].setX(i * 100 + 100);
+			robots[i].setY(500);
+		}
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		Random random = new Random();
+		boolean raceInProgress = true;
+		while (raceInProgress) {
+			for (int i = 0; i < robots.length; i++) {
+				int r = random.nextInt(300);
+				robots[i].move(r);
+				if (robots[i].getY()<0) {
+//					robots[i].sparkle();
+					raceInProgress = false;
+//					robots[i].sparkle();
+				}
+			}
+		}
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
-	//2. create an array of 5 robots.
+		// 7. declare that robot the winner and throw it a party!
+		
+		// 8. try different races with different amounts of robots.
 
-	//3. use a for loop to initialize the robots.
-
-		//4. make each robot start at the bottom of the screen, side by side, facing up
-
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+		// 9. make the robots race around a circular track.
+	}
 }
